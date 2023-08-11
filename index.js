5b6116579532b1a9c037ec232c42fdf2c3f807f0
Pavlo Grigoriev <gpavlo2020@gmail.com>
2023-08-11 19:53:16 +0300

    Update index.js
    step-3
     3.1 imports in index.js module contacts as CommonJS module from contacts.json
    3.2 check out  how  functions

---
+++ index.js
@@ -0,0 +1,26 @@
+const Contacts = require("./contacts");
+
+// Contacts.listContacts().then(console.log).catch(console.error);
+
+// Contacts.getContactById("rsKkOQUi80UsgVPCcLZZW")
+//   .then(console.log)
+//   .catch(console.error);
+
+// Contacts.removeContact("4e2caba7-5a5c-47c7-bc87-16f47158f3f2")
+//   .then(console.log)
+//   .catch(console.error);
+// backup>>>
+//  {
+//     id: 'rsKkOQUi80UsgVPCcLZZW',
+//     name: 'Alec Howard',
+//     email: 'Donec.elementum@scelerisquescelerisquedui.net',
+//     phone: '(748) 206-2688'
+//   }
+
+// Contacts.addContact({
+//   name: "Alec Howard",
+//   email: "Donec.elementum@scelerisquescelerisquedui.net",
+//   phone: "(748) 206-2688",
+// })
+//   .then(console.log)
+//   .catch(console.error);
